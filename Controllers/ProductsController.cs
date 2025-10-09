c646440d3596d726b9e276d3b68bb735e80265d8
Vinicius Queiroz <viniciusdasorte8@gmail.com>
2025-10-09 04:50:20 -0300

    feat: CRUD (PUT/DELETE) implementado e testado, Funcionalidade completa de Produto.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -10,9 +10,9 @@ public class ProdutosController : ControllerBase
     private readonly AppDbContext _context;
 
     public ProdutosController(AppDbContext context)
-        {
-            _context = context;
-        }
+    {
+        _context = context;
+    }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
@@ -22,17 +22,69 @@ public class ProdutosController : ControllerBase
     }
 
     [HttpPost]
-    public async Task<ActionResult<Produto>> PostProduto(Produto produto)    
+    public async Task<ActionResult<Produto>> PostProduto(Produto produto)
 
     {
-    _context.Produtos.Add(produto);
+        _context.Produtos.Add(produto);
 
-    await _context.SaveChangesAsync();
+        await _context.SaveChangesAsync();
 
-    return CreatedAtAction(nameof(GetProdutos), new { id = produto.Id }, produto); 
+        return CreatedAtAction(nameof(GetProdutos), new { id = produto.Id }, produto);
+
+
 
 
     }
-    
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutProduto(int id, Produto produto)
+    {
+        if (id != produto.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(produto).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        catch (DbUpdateConcurrencyException)
+
+        {
+            if (!_context.Produtos.Any(e => e.Id == id))
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                throw;
+            }
+        }
+        return NoContent();
+    }
+
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduto(int id)
+    {
+        var produto = await _context.Produtos.FindAsync(id);
+
+        if (produto == null)
+        {
+            return NotFound();
+        }
+
+        _context.Produtos.Remove(produto);
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
 }
 
+ 
